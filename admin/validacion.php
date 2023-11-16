52c8893910fe94fce1e29599ffff5d30d574d960
Yesica11 <yevari11@gmail.com>
2023-11-15 20:31:54 -0500

    cambios

--- admin/validacion.php
+++ admin/validacion.php
@@ -1,5 +1,5 @@
 <?php
-require "../includes/config/database.php";
+require "../includes/config/database.html";
 $database = conectarDataBase();
 
 if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
@@ -12,11 +12,11 @@ if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
     $query = "SELECT * FROM usuarios WHERE Nombre = '$Nombre' and Contraseña ='$contraseña'";
 
     if (mysqli_num_rows(mysqli_query($database, $query))) {
-        header ("location:../Menu_Principal_Suelo.php");
+        header ("location:../Menu_Principal_Suelo.html");
     }
     else {
         $_SESSION ["Error"] = 'EL ingreso no es valido';
-        header("location:../Login.php");
+        header("location:../Login.html");
         exit();
     }
     mysqli_close($dataBase);
